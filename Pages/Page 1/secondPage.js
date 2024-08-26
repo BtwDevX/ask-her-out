33089969e4774b0bc2b21b554378fbaad08d9a06
Dev meena <btwdevxd@gmail.com>
2024-08-26 17:13:12 +0530

    Update secondPage.js

--- Pages/Page 1/secondPage.js
+++ Pages/Page 1/secondPage.js
@@ -3,7 +3,7 @@ function hearts(){
     const container = document.querySelector('.container');
     const creat = document.createElement('div');
     creat.classList.add('hearts');
-    creat.innerHTML = '💓';
+    creat.innerHTML = '💗';
 
     creat.style.left = Math.random() * 100 + 'vw';
     creat.style.animationDuration = Math.random() * 3 + 2  + 's';
